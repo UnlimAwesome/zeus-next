93a69594c95cfd403bff36d31ca76cb78c54c720
Максим Наумов <mnauspb@yandex.ru>
2024-05-23 22:47:42 +0300

    style: create buttons added

--- src/app/dashboard/posts/layout.tsx
+++ src/app/dashboard/posts/layout.tsx
@@ -1,6 +1,12 @@
 'use client';
 
-import { Menubar, MenubarMenu, MenubarTrigger } from '@/components/ui/menubar';
+import {
+    Menubar,
+    MenubarContent,
+    MenubarItem,
+    MenubarMenu,
+    MenubarTrigger,
+} from '@/components/ui/menubar';
 
 export default function PostsLayout({
     children,
@@ -8,10 +14,14 @@ export default function PostsLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <div className='size-full'>
+        <div className='size-full p-4'>
             <Menubar className='relative h-12 px-2'>
                 <MenubarMenu>
                     <MenubarTrigger>Создать</MenubarTrigger>
+                    <MenubarContent>
+                        <MenubarItem>Продвижение компании</MenubarItem>
+                        <MenubarItem>Для пользователей</MenubarItem>
+                    </MenubarContent>
                 </MenubarMenu>
             </Menubar>
 
